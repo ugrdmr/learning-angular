6b736b1e1ad9447665a433959e4d4c1dacedfaea
Ugur Demir <ugur@codemir.ca>
2020-10-07 15:19:14 -0400

    Update content-card.component.ts

--- angularApp/src/app/content-card/content-card.component.ts
+++ angularApp/src/app/content-card/content-card.component.ts
@@ -22,28 +22,28 @@ export class ContentCardComponent implements OnInit {
     });
     this.contentList.add({
       id: 1,
-      author:'Ugur Demir',
+      author:'Ugur Demir2',
       title: 'Lorem ipsum dolor sit amet.',
       imgUrl:'https://source.unsplash.com/random/300×300',
       body:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus accumsan dolor id nunc consectetur molestie. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Quisque quis leo at erat posuere facilisis. Suspendisse malesuada nulla est, quis suscipit nisl pulvinar at. Aliquam convallis nulla non mauris viverra placerat. Donec sollicitudin ut mauris non laoreet. Curabitur feugiat mattis nulla, non tempus est iaculis eget. Cras a dolor ornare, finibus felis vitae, fermentum tortor. Praesent tincidunt in lectus ac congue. Nullam ut tempor lectus, in malesuada tellus. Curabitur ut posuere massa.'
     });
     this.contentList.add({
       id: 2,
-      author:'Ugur Demir',
+      author:'Ugur Demir3',
       title: 'Lorem ipsum dolor sit amet.',
       imgUrl:'https://source.unsplash.com/random/300×300',
       body:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus accumsan dolor id nunc consectetur molestie. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Quisque quis leo at erat posuere facilisis. Suspendisse malesuada nulla est, quis suscipit nisl pulvinar at. Aliquam convallis nulla non mauris viverra placerat. Donec sollicitudin ut mauris non laoreet. Curabitur feugiat mattis nulla, non tempus est iaculis eget. Cras a dolor ornare, finibus felis vitae, fermentum tortor. Praesent tincidunt in lectus ac congue. Nullam ut tempor lectus, in malesuada tellus. Curabitur ut posuere massa.'
     });
     this.contentList.add({
       id: 3,
-      author:'Ugur Demir',
+      author:'Ugur Demir4',
       title: 'Lorem ipsum dolor sit amet.',
       imgUrl:'https://source.unsplash.com/random/300×300',
       body:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus accumsan dolor id nunc consectetur molestie. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Quisque quis leo at erat posuere facilisis. Suspendisse malesuada nulla est, quis suscipit nisl pulvinar at. Aliquam convallis nulla non mauris viverra placerat. Donec sollicitudin ut mauris non laoreet. Curabitur feugiat mattis nulla, non tempus est iaculis eget. Cras a dolor ornare, finibus felis vitae, fermentum tortor. Praesent tincidunt in lectus ac congue. Nullam ut tempor lectus, in malesuada tellus. Curabitur ut posuere massa.'
     });
     this.contentList.add({
       id: 4,
-      author:'Ugur Demir',
+      author:'Ugur Demir5',
       title: 'Lorem ipsum dolor sit amet.',
       imgUrl:'https://source.unsplash.com/random/300×300',
       body:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus accumsan dolor id nunc consectetur molestie. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Quisque quis leo at erat posuere facilisis. Suspendisse malesuada nulla est, quis suscipit nisl pulvinar at. Aliquam convallis nulla non mauris viverra placerat. Donec sollicitudin ut mauris non laoreet. Curabitur feugiat mattis nulla, non tempus est iaculis eget. Cras a dolor ornare, finibus felis vitae, fermentum tortor. Praesent tincidunt in lectus ac congue. Nullam ut tempor lectus, in malesuada tellus. Curabitur ut posuere massa.'
